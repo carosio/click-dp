32b912497dbc70254116ce50da6b2fc842040e5d
Andreas Schultz <aschultz@tpip.net>
2015-04-01 16:16:49 +0200

    extend default test config

--- src/scg_click_dp_app.erl
+++ src/scg_click_dp_app.erl
@@ -17,7 +17,7 @@ start(_StartType, _StartArgs) ->
     Class2 = 2,
     Class3 = 3,
 
-    Groups = [{{{10,0,1,0},24},Class1},{{{10,0,2,0},24},Class2}],
+    Groups = [{{{10,0,1,0},24},Class1},{{{10,0,2,0},24},Class2},{{{10,0,3,0},24},Class3},{{{0,0,0,0},0},DEFAULT}],
 
     {ok, Pid} = scg_click_dp_app_sup:start_link(Groups),
 
@@ -25,6 +25,8 @@ start(_StartType, _StartArgs) ->
     Key1 = {inet,<<172,20,48,20>>},
     Key2 = {inet,<<172,20,48,21>>},
     Key3 = {inet,<<172,20,48,22>>},
+    Client0 = {inet,<<192,168,110,16>>},
+
     Group = DEFAULT,
     NAT = [
 	   {{1,1,1,1}, 'SymetricAddressKeyed', {2,2,2,1}},
@@ -37,12 +39,14 @@ start(_StartType, _StartArgs) ->
     Rules = [
 	     {Class1,Class2,accept},
 	     {Class2,Class1,drop},
-	     {Class1,Class3,deny}
+	     {Class1,Class3,deny},
+	     {Class1,DEFAULT,accept}
 	    ],
     click_dp:insert(Key0, {Group, NAT, Rules}),
     click_dp:insert(Key1, {Group, [],  Rules}),
     click_dp:insert(Key2, {Group, NAT, []}),
     click_dp:insert(Key3, {Group, [],  []}),
+    click_dp:insert(Client0, {Class1, [], Rules}),
     {ok, Pid}.
 
 
